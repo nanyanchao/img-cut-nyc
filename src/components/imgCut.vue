d9f3ae495004c9792450508d0c54ff88a56d5721
xiaolan <nanyanchao@coinexpress.cc>
2019-10-17 17:32:20 +0800

    update

--- src/components/imgCut.vue
+++ src/components/imgCut.vue
@@ -137,7 +137,7 @@ export default {
       ctx.clearRect(0,0,radius*2,radius*2)
       ctx.putImageData(imgData,0,0)
       this.reviewData = this.$refs['myCutImg'].toDataURL("image/png")
-      this.cutDown(this.reviewData)
+      this.$emit('cutDown',this.reviewData)
     },
   },
   mounted(){
